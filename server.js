29d29dd76bf52380e362b705797ddf78977fe68e
fede maidan <fede.maidan@gmail.com>
2017-02-24 11:04:18 -0300

    refre

--- server.js
+++ server.js
@@ -398,8 +398,9 @@ function refrescarToken() {
             expiration_date = expiration_date.getTime() + (res.body.expires_in * 1000);
             
             console.log(res.body)
-
+            console.log(user.access_token)
             user.access_token = res.body.access_token
+            console.log(user.access_token)
             user.refresh_token = res.body.refresh_token
             user.expiration_date = new Date(expiration_date)
 
