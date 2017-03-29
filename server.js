48fd7f0bb2f833f6425bbe1ca5bb0659faa3e743
fede maidan <fede.maidan@gmail.com>
2017-03-29 12:07:04 -0300

    eliminar cuenta ML

--- server.js
+++ server.js
@@ -207,8 +207,8 @@ apiRoutes.get('/usuarioML', function(req, res) {
 });
 
 apiRoutes.post('/removerUsuarioML', function(req, res) {
-   var token = getToken(req.headers);
-   console.log("token")
+   var token = getToken(req.body.headers);
+   
    console.log(token)
     if (token) {
       var decoded = jwt.decode(token, config.secret);
