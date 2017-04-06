01936dde8027784a23bc87d3ed0cff68c66c0cc2
fede maidan <fede.maidan@gmail.com>
2017-04-06 14:47:08 -0300

    mail

--- app/models/user.js
+++ app/models/user.js
@@ -51,11 +51,5 @@ UserSchema.methods.comparePassword = function (passw, cb) {
         cb(null, isMatch);
     });
 };
-
-UserSchema.path('email').validate(function (email) {
-   var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-   return emailRegex.test(email.text); // Assuming email has a text attribute
-}, 'The e-mail field cannot be empty.')
-
 module.exports = mongoose.model('User', UserSchema);
 
